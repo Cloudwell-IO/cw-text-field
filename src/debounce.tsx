33c819bd0e7de55933cfe6ada262b53f552ae9be
Troy Palacino <tpalacino@cloudwell.io>
2022-05-21 21:44:04 -0400

    Fixed issue id 1

--- src/debounce.tsx
+++ src/debounce.tsx
@@ -1,6 +1,16 @@
 import * as React from "react";
 
 /**
+ * Defines the map of debounced events.
+ */
+interface IDebounceMap {
+    /**
+     * Property name is a generated key and value is the debounce info.
+     */
+    [x: string]: number;
+}
+
+/**
  * Defines an event to debounce.
  */
 export interface IDebounceEvent {
@@ -13,6 +23,24 @@ export interface IDebounceEvent {
      * The number of milliseconds to wait before invoking the event handler.
      */
     delay: number;
+
+    /**
+     * The event handler to invoke immediately when each event occurs. The debounced event handler will still be invoked after the delay.
+     */
+    handler?: CallableFunction;
+}
+
+/**
+ * Defines the events to debounce.
+ */
+export interface IDebounceEvents {
+    /**
+     * The key can either be the name of an event to debounce or the name of an event to debounce followed by the word "Immediate" (i.e. onChangeImmediate).
+     *
+     * If the key is the name of an event to debounce, then the value must be the number of milliseconds to wait before invoking the event handler.
+     * If the key is the name of an event to debounce followed by the word "Immediate", then the value must be a CallableFunction that matches the event handler signature to be called immediately when each event occurs.
+     */
+    [key: string]: number | CallableFunction;
 }
 
 /**
@@ -22,84 +50,60 @@ export type IDebounceProps = React.PropsWithChildren<{
     /**
      * The list of events to debounce.
      */
-    events: IDebounceEvent[];
+    events: IDebounceEvent[] | IDebounceEvents;
 }>
-
-/**
- * Defines the map of debounced events.
- */
-interface IDebounceMap {
-    /**
-     * Property name is a generated key and value is the debounce info.
-     */
-    [x: string]: IDebounceInfo;
-}
-
-/**
- * Defines the debounce information.
- */
-interface IDebounceInfo {
-    /**
-     * The id of an existing delayed event handler.
-     */
-    changeId: number;
-
-    /**
-     * The delayed event handler.
-     */
-    changeHandler: Function;
-
-    /**
-     * The number of milliseconds the event handler is delayed.
-     */
-    changeDelay: number;
-}
 
 /**
  * An element that delays invoking certain event handlers.
  * @param props The properties that define the events to debounce.
  * @returns An element with delayed event handling.
  */
-export const Debounce: React.FC<IDebounceProps> = ({ events, children }) => {
+export const Debounce: React.FC<IDebounceProps> = (props) => {
 
-    /**
-     * The map of debounced events.
-     */
-    const debounceMap = React.useRef<IDebounceMap>({});
+    const eventMap = React.useRef<IDebounceMap>({});
 
-    /**
-     * Handles debouncing the event with the specified key.
-     * @param key The debounced event key.
-     * @param args The original arguments.
-     */
-    const onDebouncedEvent = (key: string, ...args: unknown[]) => {
-        const changeInfo = debounceMap.current[key];
-        if (changeInfo) {
-            if (changeInfo.changeId) {
-                window.clearTimeout(changeInfo.changeId);
-            }
-            changeInfo.changeId = window.setTimeout(() => {
-                if (changeInfo.changeHandler) {
-                    changeInfo.changeHandler(...args);
+    const getEvents = (): IDebounceEvent[] => {
+        if (Array.isArray(props.events)) {
+            return props.events;
+        }
+        else if (typeof props.events === "object") {
+            let retVal: IDebounceEvent[] = [];
+            const eventsObj: any = props.events;
+            Object.keys(eventsObj).forEach(key => {
+                if (typeof key === "string" && !key.endsWith("Immediate") && !Number.isNaN(eventsObj[key])) {
+                    retVal.push({
+                        event: key,
+                        delay: eventsObj[key],
+                        handler: eventsObj[`${key}Immediate`]
+                    });
                 }
-            }, changeInfo.changeDelay);
+            });
+            return retVal;
+        }
+        else {
+            return [];
         }
     }
 
+    const events = getEvents();
+
     return <>
-        {React.Children.map(children, (child: any, childIndex: number) => {
+        {React.Children.map(props.children, (child: any) => {
             if (events?.length > 0 && child && child.props) {
                 let newProps = { ...child.props };
                 events.forEach(e => {
                     if (e.event && e.delay >= 0 && typeof newProps[e.event] === "function") {
-                        const key = `debounce-${childIndex}-${e.event}`;
-                        debounceMap.current[key] = {
-                            changeId: 0,
-                            changeHandler: newProps[e.event],
-                            changeDelay: e.delay
-                        };
+                        const eventHandler = newProps[e.event];
                         newProps[e.event] = (...args: unknown[]) => {
-                            onDebouncedEvent(key, ...args);
+                            if (eventMap.current[e.event]) {
+                                window.clearTimeout(eventMap.current[e.event]);
+                            }
+                            eventMap.current[e.event] = window.setTimeout(() => {
+                                eventHandler(...args);
+                            }, e.delay);
+                            if (typeof e.handler === "function") {
+                                e.handler(...args);
+                            }
                         };
                     }
                 });
